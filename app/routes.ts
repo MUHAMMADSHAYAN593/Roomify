c888afb45410836e9e2c94f669f4ea035891bf40
MUHAMMADSHAYAN593 <muhammadshayanakhalq06@gmail.com>
2026-04-03 20:11:00 +0500

    Ready for Deployment

--- app/routes.ts
+++ app/routes.ts
@@ -3,8 +3,4 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 export default [
     index("routes/home.tsx"),
     route("visualizer/:id", "routes/visualizer.$id.tsx"),
-    route(
-        ".well-known/appspecific/com.chrome.devtools.json",
-        "routes/chrome-devtools.ts"
-    ),
 ] satisfies RouteConfig;
--- app/routes/chrome-devtools.ts
+++
@@ -1,9 +0,0 @@
-export async function loader() {
-  return new Response("{}", {
-    status: 200,
-    headers: {
-      "Content-Type": "application/json",
-      "Cache-Control": "no-store",
-    },
-  });
-}
